9ef68ee3446638509b4da23252f97c50f6006e6a
Daniel Garland <daniel@Daniels-MacBook-Air.local>
2013-03-31 20:59:24 -0400

    Using a YAML file to store the directory

--- student.rb
+++ student.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'yaml'
+require 'pry'
 
 class Person
   attr_accessor :name
@@ -7,6 +8,10 @@ class Person
   attr_accessor :github_user
   attr_accessor :twitter
   attr_accessor :fun_fact
+  
+  def to_s
+    "#{self.class} called #{name}"
+  end
 end
 
 class Student < Person
@@ -17,9 +22,9 @@ class Instructor < Person
   attr_accessor :type
 end
 
-@directory = ""
+@directory = []
 puts "Student Directory, v0.0.1 by Dan Garland"
-print "Enter Student or Instructor, q to save and quit: "
+print "Enter Student or Instructor, l to load, q to save and quit: "
 
 while ((input = gets.strip.chomp) != 'q') do
 
@@ -40,14 +45,22 @@ while ((input = gets.strip.chomp) != 'q') do
     person.email = gets.strip.chomp
     print "What sort of instructor are you? "
     person.type = gets.strip.chomp
+    
+  when 'l'
+    # Pull in existing people from a YAML file
+    @directory += YAML.load_documents(File.open('student_directory.yml'))
   end
   
-  # Append this to our yaml file
-  @directory += person.to_yaml
+  # Append this person to our directory
+  @directory << person
   puts @directory
   
   print "Enter Student or Instructor, q to save and quit: "
 end
 
 # Open a student_directory.yml YAML file and write it out on one line
-File.open('student_directory.yml', 'a') { |f| f.write(@directory) } 
+File.open('student_directory.yml', 'a') { |f|
+  @directory.compact.each do |person|
+    f.write(person.to_yaml)
+  end   
+} 
